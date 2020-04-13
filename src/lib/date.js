7cfd27d10bb39abceaf1c594c77d479f27e787eb
soyalatte <soyalatte@neowiz.com>
2020-04-13 17:46:40 +0900

    init function 생성 : data.js 함수 getDate와 init 으로 분리

--- src/lib/date.js
+++ src/lib/date.js
@@ -1,10 +1,15 @@
 const dateContainer = document.querySelector('.js-date')
 const dateTitle = dateContainer.querySelector('h2')
 
-export default function date() {
+function getDate() {
   const today = new Date()
   const year = today.getFullYear()
   const month = today.getMonth() + 1
   const day = today.getDate()
-  dateTitle.textContent = ` ${year}-${month}-${day} `
+  const sec = today.getSeconds();
+  dateTitle.textContent = ` ${year}-${month}-${day}- test sec: ${sec}`
+}
+
+export default function init() {
+  getDate()
 }
